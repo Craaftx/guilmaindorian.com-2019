78f8ddc9a5a247b9de0eba339519941438bc5c30
Craaftx <guilmain.dorian@gmail.com>
2019-12-12 18:49:24 +0100

    :sparkles: Add new navigation and footer

---
+++ src/components/footer.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="row">
+        <div className="col-12 text-center">
+          Copyright © - Guilmain Dorian
+          <br />
+          <small>
+            <b>Creativity is contagious. Pass it on.</b>
+          </small>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer;
---
+++ src/components/navigation.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+const Navigation = () => {
+  return (
+    <nav className="navigation">
+      <div className="navigation-brand">
+        <p>Guilmain Dorian</p>
+        <span>Developer &amp; CSS Wizard</span>
+      </div>
+      <div className="navigation-items">
+        <ul>
+          <li>About</li>
+          <li>Projects</li>
+          <li>Contact</li>
+          <li>Blog</li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
+
+export default Navigation;
