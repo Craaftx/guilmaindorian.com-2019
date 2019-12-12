94f96ef4d45998a55962d7f6d4609bc4377616b0
Craaftx <guilmain.dorian@gmail.com>
2019-12-12 18:59:55 +0100

    :wrench: Changes on meta data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Web Developer, CSS Wizard`,
+    description: `Learn more about me by exploring my personnal website created with Gatsby.js and a lot of CSS.`,
     author: `@gatsbyjs`,
   },
   plugins: [
@@ -25,7 +25,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
