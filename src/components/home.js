1d86824ee3c6ce7ba25d860c6e67e5d8d39389ee
Craaftx <guilmain.dorian@gmail.com>
2019-12-16 19:09:55 +0100

    :sparkles: Add onmousemove event

--- src/components/home.js
+++ src/components/home.js
@@ -1,19 +1,48 @@
-import React from "react"
+import React, { useState } from "react"
 
 const Home = () => {
+  const [layerOne, setLayerOne] = useState("translateX(-50%)");
+  const [layerTwo, setLayerTwo] = useState("translateX(-50%)");
+  const [layerThree, setLayerThree] = useState("translateX(-50%)");
+  const [layerFour, setLayerFour] = useState("translateX(-50%)");
+  const [layerFifth, setLayerFifth] = useState("translateX(-50%)");
+
+  const handleEvent = e => {
+    const pageX = e.clientX - window.innerWidth / 2
+    setLayerOne("translateX(-" + (50 + pageX / 200) + "%)");
+    setLayerTwo("translateX(-" + (50 + pageX / 400) + "%)");
+    setLayerThree("translateX(-" + (50 + pageX / 800) + "%)");
+    setLayerFour("translateX(-" + (50 + pageX / 1200) + "%)");
+    setLayerFifth("translateX(-" + (50 + pageX / 600) + "%)");
+  }
+
   return (
-    <section className="scene" id="scene-wrapper">
+    <section
+      className="scene"
+      id="scene-wrapper"
+      onMouseMove={e => {
+        handleEvent(e)
+      }}
+    >
       <div className="noise" />
       <div className="scene-content">
         <div className="mountains-wrapper" id="mountains-wrapper">
-          <div className="mountains" id="layer-1">
+          <div
+            className="mountains"
+            id="layer-1"
+            style={{ transform: layerOne }}
+          >
             <div className="mountain" id="mountain-1" />
             <div className="mountain" id="mountain-2" />
             <div className="mountain" id="mountain-3" />
             <div className="mountain" id="mountain-4" />
             <div className="mountain" id="mountain-5" />
           </div>
-          <div className="mountains-2" id="layer-2">
+          <div
+            className="mountains-2"
+            id="layer-2"
+            style={{ transform: layerTwo }}
+          >
             <div className="mountain" id="mountain-6" />
             <div className="mountain" id="mountain-7" />
             <div className="mountain" id="mountain-8" />
@@ -23,7 +52,11 @@ const Home = () => {
         </div>
         <div className="light" />
         <div className="planets-wrapper" id="planets-wrapper">
-          <div className="planets" id="layer-3">
+          <div
+            className="planets"
+            id="layer-3"
+            style={{ transform: layerThree }}
+          >
             <div className="planet-1">
               <div className="shadow" />
               <div className="body" />
@@ -52,7 +85,7 @@ const Home = () => {
             </div>
           </div>
         </div>
-        <ul className="stars" id="layer-4">
+        <ul className="stars" id="layer-4" style={{ transform: layerFour }}>
           <li />
           <li />
           <li />
@@ -64,7 +97,7 @@ const Home = () => {
           <li />
           <li />
         </ul>
-        <div className="rocket-wrapper d-none d-xl-block" id="layer-5">
+        <div className="rocket-wrapper">
           <div className="rocket-route" id="secret_rocket_route">
             <div className="rocket" id="secret_rocket_homepage">
               <div className="reactor" />
@@ -77,11 +110,15 @@ const Home = () => {
             </div>
           </div>
         </div>
-        <div className="text-wrapper" id="scene-text">
+        <div
+          className="text-wrapper"
+          id="scene-text"
+          style={{ transform: layerFifth }}
+        >
           <span className="animated">
             I have no special talent. I am only passionately
           </span>
-          <b className="color-primary">curious</b>
+          <b className="color-primary">&nbsp;curious</b>
         </div>
         <span className="hide-text" id="hide_text_btn">
           Hide text
