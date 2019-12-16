dfd1aa999625326a195926347c893bec46fe9d4b
Craaftx <guilmain.dorian@gmail.com>
2019-12-16 19:07:06 +0100

    :sparkles: Add new components

--- src/components/separator.js
+++ src/components/separator.js
@@ -1,3 +1,3 @@
 import React from "react";
 
-export default <div className="separator"></div>
+export default () => <div className="separator"></div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,15 +3,29 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-import Home from "../components/home";
-import About from "../components/about";
+import Separator from "../components/separator"
+import Home from "../components/home"
+import About from "../components/about"
+import Projects from "../components/projects"
+import Contact from "../components/contact"
+import RewardIndicator from "../components/rewardIndicator"
+import RewardContainer from "../components/rewardContainer"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <Home />
-    <About />
-  </Layout>
-)
+const IndexPage = () => {
+  return (
+    <Layout>
+      <SEO title="Guilmain Dorian" />
+      <Home />
+      <Separator />
+      <About style={{ animationPlayState: "paused" }} />
+      <Separator />
+      <Projects style={{ animationPlayState: "paused" }}  />
+      <Separator />
+      <Contact style={{ animationPlayState: "paused" }}  />
+      <RewardIndicator />
+      <RewardContainer />
+    </Layout>
+  )
+}
 
 export default IndexPage
