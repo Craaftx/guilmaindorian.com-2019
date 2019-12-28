1a8875d0a91e1dc3ad9298dc0a78758783cda0b1
Craaftx <guilmain.dorian@gmail.com>
2019-12-28 16:00:33 +0100

    :sparkles: Add skin selection

--- src/components/rewardContainer.js
+++ src/components/rewardContainer.js
@@ -1,15 +1,24 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+import PropTypes from "prop-types"
 
-const RewardContainer = () => {
+const RewardContainer = ({ changeSkins }) => {
+  const [opened, setOpened] = useState()
+  const [astronautSkin, setAstronautSkin] = useState()
+  const [rocketSkin, setRocketSkin] = useState()
+
+  useEffect(() => {
+    changeSkins(astronautSkin, rocketSkin)
+  }, [astronautSkin, rocketSkin])
+
   const twitterShareUrl =
     "https://twitter.com/intent/tweet?text=Hey%2C%20I%20beat%20you%20%40Craaftx1%20!!!%20%0A%0AI%20just%20found%20all%20the%20secrets%20you%27ve%20hidden%20on%20www.guilmaindorian.com%20%F0%9F%98%8E%0Apic.twitter.com%2FQh9D0Q2lO5"
 
   return (
-    <section id="reward-wrapper">
+    <section className="reward-wrapper">
       <div className="row justify-content-center">
         <div className="col-lg-8 col-md-12 text-wrapper">
           <h1 className="color-primary">Here you are !</h1>
-          <h4>Congratulations you are very curious</h4>
+          <b>Congratulations you are very curious</b>
           <p>
             You have discovered all the clues, thanks for playing with my code!
             I do not have a thousand ways to thank you, so I created a little
@@ -19,32 +28,121 @@ const RewardContainer = () => {
             href={twitterShareUrl}
             className="twitter-share-button"
             target="_blank"
-            rel="noopener noreferrer" 
+            rel="noopener noreferrer"
           >
             Share my results
           </a>
           <div className="round-wrapper">
             <div className="round" />
           </div>
-          <div id="gift-wrapper">
+          <div
+            className={opened ? "gift-wrapper opened" : "gift-wrapper"}
+            onClick={() => {
+              setOpened(true)
+            }}
+          >
             <div className="skins-wrapper">
               <h4>Astronaut Skins</h4>
-              <span id="a_skin_vanilla">Vanilla</span>
-              <span id="a_skin_explorer">Explorer</span>
-              <span id="a_skin_golden">Golden</span>
-              <span id="a_skin_night">Night</span>
-              <span id="a_skin_runic">Runic</span>
-              <span id="a_skin_invisible_skin">Invisible</span>
-              <hr />
+              <span
+                className="a_skin_vanilla"
+                onClick={() => {
+                  setAstronautSkin("skin_vanilla")
+                }}
+              >
+                Vanilla
+              </span>
+              <span
+                className="a_skin_explorer"
+                onClick={() => {
+                  setAstronautSkin("skin_explorer")
+                }}
+              >
+                Explorer
+              </span>
+              <span
+                className="a_skin_golden"
+                onClick={() => {
+                  setAstronautSkin("skin_golden")
+                }}
+              >
+                Golden
+              </span>
+              <span
+                className="a_skin_night"
+                onClick={() => {
+                  setAstronautSkin("skin_night")
+                }}
+              >
+                Night
+              </span>
+              <span
+                className="a_skin_runic"
+                onClick={() => {
+                  setAstronautSkin("skin_runic")
+                }}
+              >
+                Runic
+              </span>
+              <span
+                className="a_skin_invisible"
+                onClick={() => {
+                  setAstronautSkin("skin_invisible")
+                }}
+              >
+                Invisible
+              </span>
+              
               <h4>Rocket Skins</h4>
-              <span id="r_skin_vanilla">Vanilla</span>
-              <span id="r_skin_explorer">Explorer</span>
-              <span id="r_skin_golden">Golden</span>
-              <span id="r_skin_night">Night</span>
-              <span id="r_skin_runic">Runic</span>
-              <span id="r_skin_greyscale">Greyscale</span>
+              <span
+                className="r_skin_vanilla"
+                onClick={() => {
+                  setRocketSkin("skin_vanilla")
+                }}
+              >
+                Vanilla
+              </span>
+              <span
+                className="r_skin_explorer"
+                onClick={() => {
+                  setRocketSkin("skin_explorer")
+                }}
+              >
+                Explorer
+              </span>
+              <span
+                className="r_skin_golden"
+                onClick={() => {
+                  setRocketSkin("skin_golden")
+                }}
+              >
+                Golden
+              </span>
+              <span
+                className="r_skin_night"
+                onClick={() => {
+                  setRocketSkin("skin_night")
+                }}
+              >
+                Night
+              </span>
+              <span
+                className="r_skin_runic"
+                onClick={() => {
+                  setRocketSkin("skin_runic")
+                }}
+              >
+                Runic
+              </span>
+              <span
+                className="r_skin_greyscale"
+                onClick={() => {
+                  setRocketSkin("skin_greyscale")
+                }}
+              >
+                Greyscale
+              </span>
             </div>
-            <div className="gift" id="gift-button">
+            <div className="gift">
               <div className="background" />
               <div className="bottom" />
               <div className="top">
@@ -80,4 +178,8 @@ const RewardContainer = () => {
   )
 }
 
-export default RewardContainer;
+RewardContainer.propTypes = {
+  changeSkins: PropTypes.func,
+}
+
+export default RewardContainer
