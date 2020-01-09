7bc502e450d88661c5d418043432e3e4787a79de
Craaftx <guilmain.dorian@gmail.com>
2020-01-09 21:13:50 +0100

    :wrench: Change configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     title: `Guilmain Dorian`,
     description: `Learn more about me by exploring my personnal website created with Gatsby.js and a lot of CSS.`,
-    author: `@gatsbyjs`,
+    author: `@craaftx`,
   },
   plugins: [
     `gatsby-plugin-sass`,
@@ -14,40 +14,36 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-79846325-4",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        anonymize: true,
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Guilmain Dorian - Porfolio`,
+        short_name: `Dorian G`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#1d1d2d`,
+        theme_color: `#1d1d2d`,
         display: `minimal-ui`,
         icon: `src/images/icon.png`,
       },
     },
     {
-      resolve: 'gatsby-plugin-htaccess',
+      resolve: "gatsby-plugin-htaccess",
       options: {
         https: true,
-        host: 'guilmaindorian.com',
+        host: "guilmaindorian.com",
       },
     },
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        // The property ID; the tracking code won't be generated without it
-        trackingId: "YOUR_GOOGLE_ANALYTICS_TRACKING_ID",
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
-        anonymize: true,
-        respectDNT: true,
-        pageTransitionDelay: 0,
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-      },
-    }
   ],
 }
