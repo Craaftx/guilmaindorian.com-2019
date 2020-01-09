0c59898f3481905732f3c96b7c654c8c2b52a466
Craaftx <guilmain.dorian@gmail.com>
2020-01-09 14:02:37 +0100

    :heavy_minus_sign: Remove offline plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,11 +25,15 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/icon.png`,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: 'gatsby-plugin-htaccess',
+      options: {
+        https: true,
+        host: 'guilmaindorian.com',
+      },
+    },
   ],
 }
